6194c7685c21313f0ccdc4c24478a7bf4a45734c
DuyPh1472 <saophadao1472@gmail.com>
2024-12-24 16:19:44 +0700

    fix throw exception for category

--- Monhealth.Application/Features/Category/AddCategory/AddCategoryHandler.cs
+++ Monhealth.Application/Features/Category/AddCategory/AddCategoryHandler.cs
@@ -22,7 +22,7 @@ namespace Monhealth.Application.Features.Category.AddCategory
             if (isCategoryExist)
             {
                 // Trả về lỗi không hợp lệ
-                throw new Exception("CategoryName already exists.");
+                throw new Exception("Danh mục đã tồn tại.");
             }
             var model = new Monhealth.Domain.Category
             {
