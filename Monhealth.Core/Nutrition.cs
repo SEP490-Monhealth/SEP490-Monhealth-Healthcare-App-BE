8eedc19db47f38f1eec4cd275f64a379da04bf60
DuyPh1472 <saophadao1472@gmail.com>
2024-12-25 15:26:16 +0700

    add more field for nutrition

--- Monhealth.Core/Nutrition.cs
+++ Monhealth.Core/Nutrition.cs
@@ -15,7 +15,19 @@ namespace Monhealth.Domain
         public float Fat { get; set; }
         public float Fiber { get; set; }
         public float Sugar { get; set; }
-
+        public float? SaturatedFat { get; set; }
+        public float? UnsaturatedFat { get; set; }
+        public float? Cholesterol { get; set; }
+        public float? Sodium { get; set; }
+        public float? Potassium { get; set; }
+        public float? Calcium {get; set;}
+        public float? Iron {get; set;}
+        public float? VitaminA { get; set; }
+        public float? VitaminB1 { get; set; }
+        public float? VitaminB2 { get; set; }
+        public float? VitaminC { get; set; }
+        public float? VitaminD { get; set; }
+        public float? VitaminE { get; set; }
         [ForeignKey(nameof(FoodId))]
         public Food Food { get; set; }
     }
