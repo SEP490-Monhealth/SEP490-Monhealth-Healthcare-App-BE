4a14f69eeacac8d52e3b011f03b71e44be44b277
DuyPh1472 <saophadao1472@gmail.com>
2024-12-25 21:06:39 +0700

    fix respone for nutrition

--- Monhealth.Application/Features/Nutrition/Queries/GetAllNutritionByfoodId/NutritionByFoodIdDto.cs
+++ Monhealth.Application/Features/Nutrition/Queries/GetAllNutritionByfoodId/NutritionByFoodIdDto.cs
@@ -5,7 +5,6 @@ namespace Monhealth.Application.Features.Nutrition.Queries.GetAllNutrition
     public class NutritionByFoodIdDto : BaseEntity
     {
         public Guid NutritionId { get; set; }
-        public Guid? UserFoodId { get; set; }
         public Guid? FoodId { get; set; }
         public float Calories { get; set; }
         public float Protein { get; set; }
@@ -13,5 +12,19 @@ namespace Monhealth.Application.Features.Nutrition.Queries.GetAllNutrition
         public float Fat { get; set; }
         public float Fiber { get; set; }
         public float Sugar { get; set; }
+        public float? SaturatedFat { get; set; }
+        public float? UnsaturatedFat { get; set; }
+        public float? Cholesterol { get; set; }
+        public float? Sodium { get; set; }
+        public float? Potassium { get; set; }
+        public float? Calcium { get; set; }
+        public float? Iron { get; set; }
+        public float? VitaminA { get; set; }
+        public float? VitaminB1 { get; set; }
+        public float? VitaminB2 { get; set; }
+        public float? VitaminC { get; set; }
+        public float? VitaminD { get; set; }
+        public float? VitaminE { get; set; }
+
     }
 }
--- Monhealth.Identity/Dbcontexts/DataSeeder.cs
+++ Monhealth.Identity/Dbcontexts/DataSeeder.cs
@@ -14,7 +14,7 @@ namespace Monhealth.Identity.Dbcontexts
             {
                 await context.Roles.AddAsync(new AppRole()
                 {
-                    Id = rootAdminRoleId,
+                Id = rootAdminRoleId,
                     Name = "Admin",
                     NormalizedName = "ADMIN",
                     DisplayName = "Quản trị viên"
