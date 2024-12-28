485207ebfcb1af772e9642294ee645fe4eb3aaeb
NPKhanh <khanh01668309526@gmail.com>
2024-12-28 11:20:07 +0700

    commit updateMetric

--- Monhealth.Application/Features/Metric/Commands/UpdateMetric/UpdateMetricCommandHandler.cs
+++ Monhealth.Application/Features/Metric/Commands/UpdateMetric/UpdateMetricCommandHandler.cs
@@ -28,6 +28,35 @@ namespace Monhealth.Application.Features.Metric.Commands.UpdateMetric
             metricToUpdate.UpdatedAt = DateTime.Now;
             // map DTO to entity
             _mapper.Map(request.MetricDto, metricToUpdate);
+
+            // tinh tuoi tu DateOfBirth
+            var age = DateTime.Now.Year - request.MetricDto.DateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < request.MetricDto.DateOfBirth.DayOfYear)
+            {
+                age--;
+            }
+
+            // kiem tra va cap nhat cac gia tri bmi, bmr, tdee, ibw
+            if (request.MetricDto.DateOfBirth != metricToUpdate.DateOfBirth ||
+                request.MetricDto.Gender != metricToUpdate.Gender.ToString() ||
+                request.MetricDto.Height != metricToUpdate.Height ||
+                request.MetricDto.Weight != metricToUpdate.Weight ||
+                request.MetricDto.ActivityLevel != metricToUpdate.ActivityLevel)
+            {
+                var metricsCalculate = new UpdateMetricsCalculate();
+                // tinh lai bmi
+                metricToUpdate.Bmi = (float)metricsCalculate.CalculateBMI(request.MetricDto.Weight, request.MetricDto.Height);
+
+                // tinh lai bmr
+                metricToUpdate.Bmr = metricsCalculate.CalculateBMR(request.MetricDto.Weight, request.MetricDto.Height, age, request.MetricDto.Gender);
+
+                // tinh lai tdee
+                metricToUpdate.Tdee = metricsCalculate.CalculateTDEE(metricToUpdate.Bmr, request.MetricDto.ActivityLevel);
+
+                // tinh lai ibw
+                metricToUpdate.Ibw = metricsCalculate.CalculateIBW(request.MetricDto.Height, request.MetricDto.Gender);
+            }
+
             _metricRepository.Update(metricToUpdate);
             await _metricRepository.SaveChangeAsync();
             return true;
