baa860d45deb9f0e685d2e86ff92f15cccd77858
DuyPh1472 <saophadao1472@gmail.com>
2024-12-29 00:16:10 +0700

    fix respone for Create Meal

--- Monhealth.Application/Features/Meal/Commands/CreateMeal/CreateMealDTO.cs
+++ Monhealth.Application/Features/Meal/Commands/CreateMeal/CreateMealDTO.cs
@@ -17,8 +17,11 @@ namespace Monhealth.Application.Features.Meal.Commands.CreateMeal
     {
         public Guid FoodId { get; set; }
         public int Quantity { get; set; }
+        [JsonPropertyName("size")]
         public string PortionSize { get; set; } = string.Empty;
+        [JsonPropertyName("weight")]
         public float PortionWeight { get; set; }
+        [JsonPropertyName("unit")]
         public string MeasurementUnit { get; set; } = string.Empty;
 
     }
