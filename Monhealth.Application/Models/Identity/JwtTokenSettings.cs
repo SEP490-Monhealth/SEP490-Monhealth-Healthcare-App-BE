f53d647c05517d0c9dc5ae019eacb487872dfd5d
QuocDai <145921869+dainq94@users.noreply.github.com>
2024-12-30 21:17:06 +0700

    expire token more 30 days

--- Monhealth.Application/Models/Identity/JwtTokenSettings.cs
+++ Monhealth.Application/Models/Identity/JwtTokenSettings.cs
@@ -4,6 +4,6 @@
     {
         public string Key { get; set; }
         public string Issuer { get; set; }
-        public int ExpireInHours { get; set; }
+        public int ExpireInDays { get; set; }
     }
 }
