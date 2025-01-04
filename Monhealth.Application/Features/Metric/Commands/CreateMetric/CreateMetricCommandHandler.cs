beceff1375500c0e1bb18c66685d6d11cbe1717d
QuocDai <145921869+dainq94@users.noreply.github.com>
2025-01-04 22:34:17 +0700

    fix handler in metric

--- Monhealth.Application/Features/Metric/Commands/CreateMetric/CreateMetricCommandHandler.cs
+++ Monhealth.Application/Features/Metric/Commands/CreateMetric/CreateMetricCommandHandler.cs
@@ -10,26 +10,19 @@ namespace Monhealth.Application.Features.Metric.Commands.CreateMetric
         private readonly IMetricsCalculate _metricCalculate;
         private readonly IMetricRepository _metricRepository;
         private readonly IMapper _mapper;
-        public CreateMetricCommandHandler(IMetricRepository metricRepository, IMapper mapper, IMetricsCalculate metricCalculate)
         private readonly IGenerateDailyMenuService _generateDailyMenuService;
         private readonly IDailyMealRepository _dailyMealRepository;
-        public CreateMetricCommandHandler(IMetricRepository metricRepository, IMapper mapper, IGenerateDailyMenuService generateDailyMenuService, IDailyMealRepository dailyMealRepository)
+        public CreateMetricCommandHandler(IMetricRepository metricRepository, IMapper mapper, IGenerateDailyMenuService generateDailyMenuService, IDailyMealRepository dailyMealRepository, IMetricsCalculate metricCalculate)
         {
-            _metricCalculate = metricCalculate;
             _metricRepository = metricRepository;
             _mapper = mapper;
             _generateDailyMenuService = generateDailyMenuService;
             _dailyMealRepository = dailyMealRepository;
+            _metricCalculate = metricCalculate;
         }
         public async Task<Unit> Handle(CreateMetricCommand request, CancellationToken cancellationToken)
         {
-
-
-            var newMetric = _mapper.Map<Monhealth.Domain.Metric>(request.CreateMetricDto);
-
-            // tinh tuoi tu DateOfBirth
-            var age = DateTime.Now.Year - request.CreateMetricDto.DateOfBirth.Year;
-            if (DateTime.Now.DayOfYear < request.CreateMetricDto.DateOfBirth.DayOfYear)
+            try
             {
                 var newMetric = _mapper.Map<Monhealth.Domain.Metric>(request.CreateMetricDto);
 
@@ -73,48 +66,8 @@ namespace Monhealth.Application.Features.Metric.Commands.CreateMetric
             {
                 throw new Exception("Lỗi create " + ex.Message);
             }
-                age--;
-            }
-
-            // tinh toan BMI, BMR, TDEE, IBW
-            var calculator = new MetricsCalculate();
-            var weight = request.CreateMetricDto.Weight;
-            var height = request.CreateMetricDto.Height;
-            var gender = request.CreateMetricDto.Gender;
-            var activityLevel = request.CreateMetricDto.ActivityLevel;
-
-            // tinh BMI
-            newMetric.Bmi = (float)calculator.CalculateBMI(weight, height);
-
-            // tinh BMR
-            var bmr = calculator.CalculateBMR(weight, height, age, gender);
-            newMetric.Bmr = bmr;
-
-            // tinh TDEE
-            newMetric.Tdee = calculator.CalculateTDEE(bmr, activityLevel);
-
-            // tinh IBW
-            newMetric.Ibw = calculator.CalculateIBW(height, gender);
-
-
-            // new metricId
-            newMetric.MetricId = Guid.NewGuid();
-            newMetric.CreatedAt = DateTime.Now;
-            newMetric.UpdatedAt = DateTime.Now;
-
-            _metricRepository.Add(newMetric);
-
-            var dailyMeal = await _generateDailyMenuService.GenerateDailyMealAsync(request.CreateMetricDto.UserId, newMetric.Tdee, "lose");
-            _dailyMealRepository.Add(dailyMeal);
-
-            await _metricRepository.SaveChangeAsync();
-
-
-
-            return Unit.Value;
-
-
 
         }
+
     }
 }
