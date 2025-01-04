5988a6e69f2a1328b34c9f09b5adef18f8e482ec
QuocDai <145921869+dainq94@users.noreply.github.com>
2025-01-04 22:38:23 +0700

    Add logic recommend in hanlder

--- Monhealth.Application/Features/Metric/Commands/CreateMetric/CreateMetricCommandHandler.cs
+++ Monhealth.Application/Features/Metric/Commands/CreateMetric/CreateMetricCommandHandler.cs
@@ -22,50 +22,51 @@ namespace Monhealth.Application.Features.Metric.Commands.CreateMetric
         }
         public async Task<Unit> Handle(CreateMetricCommand request, CancellationToken cancellationToken)
         {
-            try
+
+
+            var newMetric = _mapper.Map<Monhealth.Domain.Metric>(request.CreateMetricDto);
+
+            // tinh tuoi tu DateOfBirth
+            var age = DateTime.Now.Year - request.CreateMetricDto.DateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < request.CreateMetricDto.DateOfBirth.DayOfYear)
             {
-                var newMetric = _mapper.Map<Monhealth.Domain.Metric>(request.CreateMetricDto);
-
-                // tinh tuoi tu DateOfBirth
-                var age = DateTime.Now.Year - request.CreateMetricDto.DateOfBirth.Year;
-                if (DateTime.Now.DayOfYear < request.CreateMetricDto.DateOfBirth.DayOfYear)
-                {
-                    age--;
-                }
-
-                // tinh toan BMI, BMR, TDEE, IBW
-                var weight = request.CreateMetricDto.Weight;
-                var height = request.CreateMetricDto.Height;
-                var gender = request.CreateMetricDto.Gender;
-                var activityLevel = request.CreateMetricDto.ActivityLevel;
-
-                // tinh BMI
-                newMetric.Bmi = (float)_metricCalculate.CalculateBMI(weight, height);
-
-                // tinh BMR
-                var bmr = _metricCalculate.CalculateBMR(weight, height, age, gender);
-                newMetric.Bmr = bmr;
-
-                // tinh TDEE
-                newMetric.Tdee = _metricCalculate.CalculateTDEE(bmr, activityLevel);
-
-                // tinh IBW
-                newMetric.Ibw = _metricCalculate.CalculateIBW(height, gender);
-
-
-                // new metricId
-                newMetric.MetricId = Guid.NewGuid();
-                newMetric.CreatedAt = DateTime.Now;
-                newMetric.UpdatedAt = DateTime.Now;
-
-                _metricRepository.Add(newMetric);
-                await _metricRepository.SaveChangeAsync();
-                return Unit.Value;
+                age--;
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Lỗi create " + ex.Message);
-            }
+
+            // tinh toan BMI, BMR, TDEE, IBW
+            var weight = request.CreateMetricDto.Weight;
+            var height = request.CreateMetricDto.Height;
+            var gender = request.CreateMetricDto.Gender;
+            var activityLevel = request.CreateMetricDto.ActivityLevel;
+
+            // tinh BMI
+            newMetric.Bmi = (float)_metricCalculate.CalculateBMI(weight, height);
+
+            // tinh BMR
+            var bmr = _metricCalculate.CalculateBMR(weight, height, age, gender);
+            newMetric.Bmr = bmr;
+
+            // tinh TDEE
+            newMetric.Tdee = _metricCalculate.CalculateTDEE(bmr, activityLevel);
+
+            // tinh IBW
+            newMetric.Ibw = _metricCalculate.CalculateIBW(height, gender);
+
+
+            // new metricId
+            newMetric.MetricId = Guid.NewGuid();
+            newMetric.CreatedAt = DateTime.Now;
+            newMetric.UpdatedAt = DateTime.Now;
+
+            _metricRepository.Add(newMetric);
+
+            var dailyMeal = await _generateDailyMenuService.GenerateDailyMealAsync(request.CreateMetricDto.UserId, newMetric.Tdee, "lose");
+            _dailyMealRepository.Add(dailyMeal);
+
+            await _metricRepository.SaveChangeAsync();
+            return Unit.Value;
+
+
 
         }
 
