412836a2d613bd3bd81df3b7dc19259c5f268960
DuyPh1472 <saophadao1472@gmail.com>
2025-01-09 13:21:08 +0700

    add more field in request create food

--- Monhealth.Application/Features/Food/AddFoodUser/AddFoodUserRequest.cs
+++ Monhealth.Application/Features/Food/AddFoodUser/AddFoodUserRequest.cs
@@ -29,5 +29,18 @@ namespace Monhealth.Application.Features.Food.AddFoodUser
         public float Fat { get; set; }
         public float Fiber { get; set; }
         public float Sugar { get; set; }
+        public float? SaturatedFat { get; set; }
+        public float? UnsaturatedFat { get; set; }
+        public float? Cholesterol { get; set; }
+        public float? Sodium { get; set; }
+        public float? Potassium { get; set; }
+        public float? Calcium { get; set; }
+        public float? Iron { get; set; }
+        public float? VitaminA { get; set; }
+        public float? VitaminB1 { get; set; }
+        public float? VitaminB2 { get; set; }
+        public float? VitaminC { get; set; }
+        public float? VitaminD { get; set; }
+        public float? VitaminE { get; set; }
     }
 }
