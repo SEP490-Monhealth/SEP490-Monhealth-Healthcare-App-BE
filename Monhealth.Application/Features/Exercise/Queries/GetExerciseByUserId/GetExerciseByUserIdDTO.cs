99d58b18bf29e0bf1e1350867eeed4e79f76140d
NgPhKhanh <khanh01668309526@gmail.com>
2025-02-10 16:57:35 +0700

    fix get Exercise

--- Monhealth.Application/Features/Exercise/Queries/GetExerciseByUserId/GetExerciseByUserIdDTO.cs
+++ Monhealth.Application/Features/Exercise/Queries/GetExerciseByUserId/GetExerciseByUserIdDTO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Monhealth.Domain.Enum;
 
 namespace Monhealth.Application.Features.Exercise.Queries.GetExerciseByUserId
 {
@@ -11,16 +12,11 @@ namespace Monhealth.Application.Features.Exercise.Queries.GetExerciseByUserId
     {
         public Guid ExerciseId { get; set; }
         [JsonPropertyName("category")]
-        public string CategoryName { get; set; } = string.Empty;
+        public ExerciseType ExerciseType { get; set; }
         [JsonPropertyName("name")]
         public string ExerciseName { get; set; } = string.Empty;
-        [JsonPropertyName("description")]
-        public string ExerciseDescription { get; set; } = string.Empty;
-        public int Duration { get; set; }
-        public float CaloriesBurned { get; set; }
         public string Instructions { get; set; } = string.Empty;
-        public string IntensityLevel { get; set; } = string.Empty;
-        public string DifficultyLevel { get; set; } = string.Empty;
+        public float CaloriesPerMinute { get; set; }
         public bool Status { get; set; }
     }
 }
