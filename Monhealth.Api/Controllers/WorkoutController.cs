7023218033f20ee0123a6109d3e42e45b4ae60cd
QuocDai <145921869+dainq94@users.noreply.github.com>
2025-02-12 21:26:37 +0700

    fixed confict workout controller

--- Monhealth.Api/Controllers/WorkoutController.cs
+++ Monhealth.Api/Controllers/WorkoutController.cs
@@ -1,21 +1,17 @@
-﻿namespace Monhealth.Api.Controllers
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Monhealth.Application.Features.Workout.Commands.CreateWorkout;
+using Monhealth.Application.Features.Workout.Queries.GetAllWorkoutQueries;
+using Monhealth.Application.Features.Workout.Queries.GetWorkoutByIdQueries;
+using Monhealth.Application.Models;
+using Monhealth.Core.Enum;
+
+namespace Monhealth.Api.Controllers
 {
     [Route("api/v1/workouts")]
     [ApiController]
     public class WorkoutController(IMediator mediator) : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<ResultModel>> GetAllWorkouts(int page = 1, int limit = 10, string? category = null, string? search = null, DifficultyLevel? difficulty = null, bool? popular = null, bool? status = null)
-        {
-            var workouts = await mediator.Send(new GetAllWorkoutQuery(page, limit, category, search, difficulty, popular, status));
-
-            return new ResultModel
-            {
-                Data = workouts,
-                Status = 200,
-                Success = true,
-            };
-        }
 
         [HttpPost]
         public async Task<ActionResult<ResultModel>> CreateWorkout([FromBody] CreateWorkoutCommand command)
@@ -38,6 +34,7 @@
                 Status = 400,
             });
         }
+
         [HttpGet]
         public async Task<ActionResult<ResultModel>> GetAllWorkouts(int page = 1, int limit = 10, string? category = null, string? search = null, DifficultyLevel? difficulty = null, WorkoutType? type = null, bool? popular = null, bool? status = null)
         {
