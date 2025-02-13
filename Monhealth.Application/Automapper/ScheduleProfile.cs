215d1f1d1786735d65712099ee2a764d1625a8b1
DuyPh1472 <saophadao1472@gmail.com>
2025-02-13 16:21:27 +0700

    feat: Get Schedule By Id

--- Monhealth.Application/Automapper/ScheduleProfile.cs
+++ Monhealth.Application/Automapper/ScheduleProfile.cs
@@ -11,8 +11,35 @@ namespace Monhealth.Application.Automapper
         public ScheduleProfile()
         {
             CreateMap<ScheduleDTO, Schedule>().ReverseMap();
-            CreateMap<ScheduleDetailDTO, Schedule>().ReverseMap();
-            CreateMap<GetScheduleByUserDTO , Schedule>().ReverseMap();
+            CreateMap<ScheduleDetailDTO, Schedule>()
+     // Map ConsultantId from ScheduleDetailDTO to Schedule
+     .ForMember(dest => dest.ConsultantId,
+         opt => opt.MapFrom(src => src.ConsultantId))
+
+     // Map Date from ScheduleDetailDTO to Schedule
+     .ForMember(dest => dest.Date,
+         opt => opt.MapFrom(src => src.Date))
+
+     // Map BookedSlots from ScheduleDetailDTO to Schedule
+     .ForMember(dest => dest.BookedSlots,
+         opt => opt.MapFrom(src => src.BookedSlots))
+
+     // Map StartTime from ScheduleDetailDTO to Schedule
+     .ForMember(dest => dest.StartTime,
+         opt => opt.MapFrom(src => src.StartTime))
+
+     // Map EndTime from ScheduleDetailDTO to Schedule
+     .ForMember(dest => dest.EndTime,
+         opt => opt.MapFrom(src => src.EndTime))
+
+     // Map MaxBookings from ScheduleDetailDTO to Schedule
+     .ForMember(dest => dest.MaxBookings,
+         opt => opt.MapFrom(src => src.MaxBookings))
+
+     // Reverse map: Automatically map from Schedule to ScheduleDetailDTO in reverse
+     .ReverseMap();
+
+            CreateMap<GetScheduleByUserDTO, Schedule>().ReverseMap();
         }
 
 
--- Monhealth.Application/Features/Schedule/Queries/GetById/ScheduleDetailDTO.cs
+++ Monhealth.Application/Features/Schedule/Queries/GetById/ScheduleDetailDTO.cs
@@ -4,8 +4,8 @@ namespace Monhealth.Application.Features.Subscription.Queries.GetById
     {
         public Guid ConsultantId { get; set; } // FK 
         public DateOnly? Date { get; set; } // 
-        public TimeOnly StartTime { get; set; }
-        public TimeOnly EndTime { get; set; } //
+        public string StartTime { get; set; }
+        public string EndTime { get; set; } //
         public int MaxBookings { get; set; }
         public int BookedSlots { get; set; }
     }
