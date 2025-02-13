9be0ffd2e9a3c415d13d724b56e3d00cfbd60402
NgPhKhanh <khanh01668309526@gmail.com>
2025-02-13 13:04:24 +0700

    add consultant table and add more field for service table

---
+++ Monhealth.Core/Consultants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Monhealth.Domain.Common;
+using Monhealth.Identity.Models;
+
+namespace Monhealth.Domain
+{
+    public class Consultants : TimeEntity
+    {
+        public Guid ConsultantId { get; set; }
+        public Guid UserId { get; set; }
+        public string Bio { get; set; } = string.Empty;
+        public string Expertise { get; set; } = string.Empty;
+        public int Experience { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public AppUser AppUser { get; set; }
+    }
+}
--- Monhealth.Core/Service.cs
+++ Monhealth.Core/Service.cs
@@ -1,4 +1,5 @@
 ﻿using Monhealth.Domain.Common;
+using Monhealth.Domain.Enum;
 using Monhealth.Identity.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -10,12 +11,22 @@ namespace Monhealth.Domain
         [Key]
         public Guid ServiceId { get; set; } // PK 
         public Guid ConsultantId { get; set; }
+        public Guid CategoryId { get; set; }
+        public ServiceType ServiceType { get; set; }
         public string ServiceName { get; set; } = string.Empty;
         public string ServiceDescription { get; set; } = string.Empty;
         public int Price { get; set; }
+        public int Duration { get; set; }
+        public bool Status { get; set; }
+        public string OnlineLink { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public bool VisitHome { get; set; }
+        public int MaxDistance { get; set; }
 
         [ForeignKey(nameof(ConsultantId))]
         public AppUser Consultant { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
 
         public ICollection<Booking> Bookings { get; set; }
 
