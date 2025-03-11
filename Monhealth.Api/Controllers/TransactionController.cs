402f7ab032321a35527ac8be4954e6fb717465ba
NgPhKhanh <khanh01668309526@gmail.com>
2025-03-11 08:14:02 +0700

    done delete transaction

--- Monhealth.Api/Controllers/TransactionController.cs
+++ Monhealth.Api/Controllers/TransactionController.cs
@@ -3,7 +3,9 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Monhealth.Application.Features.Consultant.Commands.CreateConsultant;
+using Monhealth.Application.Features.Consultant.Commands.DeleteConsultant;
 using Monhealth.Application.Features.Transaction.Commands.CreateTransaction;
+using Monhealth.Application.Features.Transaction.Commands.DeleteTransaction;
 using Monhealth.Application.Models;
 
 namespace Monhealth.Api.Controllers
@@ -31,6 +33,27 @@ namespace Monhealth.Api.Controllers
                 Message = "Tạo thanh toán thất bại",
                 Status = (int)HttpStatusCode.BadRequest,
                 Success = false
+            };
+        }
+        [HttpDelete("{transactionId}")]
+        public async Task<ActionResult<ResultModel>> DeleteTransaction(Guid transactionId)
+        {
+            var command = new DeleteTransactionCommand { TransactionId = transactionId };
+            var delete = await mediator.Send(command);
+            if (!delete)
+            {
+                return new ResultModel
+                {
+                    Success = false,
+                    Status = (int)HttpStatusCode.NotFound,
+                    Message = "Không tìm thấy giao dịch"
+                };
+            }
+            return new ResultModel
+            {
+                Success = true,
+                Status = (int)HttpStatusCode.OK,
+                Message = "Xóa giao dịch thành công"
             };
         }
     }
