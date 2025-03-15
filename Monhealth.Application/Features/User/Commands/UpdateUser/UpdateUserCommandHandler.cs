e8b0b44e0650bc68cf9b22f5d261112975286d9a
DuyPh1472 <saophadao1472@gmail.com>
2025-03-16 00:40:54 +0700

    fix: Update user

--- Monhealth.Application/Features/User/Commands/UpdateUser/UpdateUserCommandHandler.cs
+++ Monhealth.Application/Features/User/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -17,25 +17,36 @@ namespace Monhealth.Application.Features.User.Commands.UpdateUser
         public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
             var user = await _userRepository.GetUserByUserId(request.UserId);
-            if (await _userRepository.IsPhoneNumberDuplicateAsync(request.PhoneNumber))
-            {
-                throw new BadRequestException("Số điện thoại đã được sử dụng");
-            }
-            if (await _userRepository.IsEmailDuplicatedAsync(request.Email))
-            {
-                throw new BadRequestException("Email đã được sử dụng");
-            }
             if (user == null)
             {
                 throw new NotFoundException("Không tìm thấy người dùng");
 
             }
+            if (request.UserId == user.Id)
+            {
+                request.PhoneNumber = user.PhoneNumber!;
+                request.Email = user.Email!;
+                user.NormalizedEmail = request.Email.ToUpper();
+            }
+            else
+            {
+
+                if (await _userRepository.IsPhoneNumberDuplicateAsync(request.PhoneNumber))
+                {
+                    throw new BadRequestException("Số điện thoại đã được sử dụng");
+                }
+                if (await _userRepository.IsEmailDuplicatedAsync(request.Email))
+                {
+                    throw new BadRequestException("Email đã được sử dụng");
+                }
+                request.PhoneNumber = user.PhoneNumber!;
+                request.Email = user.Email!;
+                user.NormalizedEmail = request.Email.ToUpper();
+            }
             user.FullName = request.FullName;
-            user.Email = request.Email;
-            user.NormalizedEmail = request.Email.ToUpper();
-            user.PhoneNumber = request.PhoneNumber;
+
             _userRepository.Update(user);
-            _userRepository.SaveChangesAsync();
+            await _userRepository.SaveChangesAsync();
             return Unit.Value;
         }
     }
