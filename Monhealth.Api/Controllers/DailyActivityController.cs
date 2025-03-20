eda7c7445789d1ddd4f0ac772bf3f0f6e3b4446d
NgPhKhanh <khanh01668309526@gmail.com>
2025-03-20 14:04:52 +0700

    done GetDailyActivityByUserId

---
+++ Monhealth.Api/Controllers/DailyActivityController.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Monhealth.Application.Features.DailyActivity.Queries.GetDailyActivityByUserId;
+using Monhealth.Application.Features.DailyMeal.Queries.GetDailyMealForUser;
+using Monhealth.Application.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Monhealth.Api.Controllers
+{
+    [Route("api/v1/daily-activities")]
+    [ApiController]
+    public class DailyActivityController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet]
+        [Route("user")]
+        [SwaggerOperation(Summary = "Done")]
+        public async Task<ActionResult<ResultModel>> GetDailyActivityByUserId([FromQuery][Required] Guid userId,
+         [FromQuery][Required] DateTime date)
+        {
+            var queries = await mediator.Send(new GetDailyActivityByUserIdQuery(userId, date));
+
+            if (queries == null)
+            {
+                return Ok(new ResultModel
+                {
+                    Success = false,
+                    Message = "Hoạt động hằng ngày không tồn tại",
+                    Status = 200,
+                    Data = queries
+                });
+            }
+
+            return Ok(new ResultModel
+            {
+                Success = true,
+                Status = 200,
+                Data = queries
+            });
+        }
+    }
+}
