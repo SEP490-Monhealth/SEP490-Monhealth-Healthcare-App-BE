00640095919f432b5095f9c107a674c9353a0cf0
DuyPh1472 <saophadao1472@gmail.com>
2025-03-20 22:24:35 +0700

    feat: Add pagination for category

--- Monhealth.Application/Features/Category/Queries/GetAllCategories/GetCategoryListQuery.cs
+++ Monhealth.Application/Features/Category/Queries/GetAllCategories/GetCategoryListQuery.cs
@@ -1,10 +1,23 @@
 ﻿using MediatR;
 using Monhealth.Application.Features.Category.Queries.GetAllCategories;
+using Monhealth.Application.Models.Paging;
+using Monhealth.Core.Enum;
 
 namespace Monhealth.Application.Features.Metric.Queries.GetAllMetric
 {
-    public class GetCategoryListQuery : IRequest<List<CategoryDTO>>
+    public class GetCategoryListQuery : IRequest<PageResult<CategoryDTO>>
     {
-        
+        public int Page { get; set; }
+        public int Limit { get; set; }
+        public string Search { get; set; } = string.Empty;
+        public CategoryType? Type { get; set; }
+        public GetCategoryListQuery(int page, int limit, string search, CategoryType? type)
+        {
+            Page = page;
+            Limit = limit;
+            Search = search;
+            Type = type;
+        }
+
     }
 }
--- Monhealth.Application/Features/Category/Queries/GetAllCategories/GetCategoryListQueryHandler.cs
+++ Monhealth.Application/Features/Category/Queries/GetAllCategories/GetCategoryListQueryHandler.cs
@@ -2,10 +2,11 @@
 using MediatR;
 using Monhealth.Application.Contracts.Persistence;
 using Monhealth.Application.Features.Category.Queries.GetAllCategories;
+using Monhealth.Application.Models.Paging;
 
 namespace Monhealth.Application.Features.Metric.Queries.GetAllMetric
 {
-    public class GetCategoryListQueryHandler : IRequestHandler<GetCategoryListQuery, List<CategoryDTO>>
+    public class GetCategoryListQueryHandler : IRequestHandler<GetCategoryListQuery, PageResult<CategoryDTO>>
     {
         private readonly IMapper _mapper;
         private readonly ICategoryRepository _categoryRepository;
@@ -14,10 +15,30 @@ namespace Monhealth.Application.Features.Metric.Queries.GetAllMetric
             _mapper = mapper;
             _categoryRepository = categoryRepository;
         }
-        public async Task<List<CategoryDTO>> Handle(GetCategoryListQuery request, CancellationToken cancellationToken)
+        public async Task<PageResult<CategoryDTO>> Handle(GetCategoryListQuery request, CancellationToken cancellationToken)
         {
-            var categories = await _categoryRepository.GetAllCategoryAsync();
-            return _mapper.Map<List<CategoryDTO>>(categories);
+         
+            var categories = await _categoryRepository
+             .GetAllCategoryAsync(request.Page, request.Limit, request.Search, request.Type);
+            var categoryList = categories.Items.Select(c => new CategoryDTO
+            {
+                CategoryId = c.CategoryId,
+                CategoryName = c.CategoryName,
+                CategoryType = c.CategoryType,
+                CreatedAt = c.CreatedAt,
+                UpdatedAt = c.UpdatedAt,
+                CategoryDescription = c.CategoryDescription ?? string.Empty,
+
+            }).ToList();
+
+            return new PageResult<CategoryDTO>
+            {
+                CurrentPage = request.Page,
+                TotalPages = (int)Math.Ceiling(categories.TotalCount / (double)request.Limit),
+                TotalItems = categories.TotalCount,
+                Items = categoryList
+            };
+
         }
     }
 }
