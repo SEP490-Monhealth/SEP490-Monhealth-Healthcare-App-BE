2743d051b1b695b59b11e29a1f50f71385dff449
DuyPh1472 <saophadao1472@gmail.com>
2025-03-22 11:26:04 +0700

    fix: Remove field cancellationReason

--- Monhealth.Application/Features/Booking/Commands/CreateBooking/CreateBookingCommand.cs
+++ Monhealth.Application/Features/Booking/Commands/CreateBooking/CreateBookingCommand.cs
@@ -8,6 +8,5 @@ namespace Monhealth.Application.Features.Booking.Commands.CreateBooking
         public Guid? ConsultantId { get; set; }
         public DateTime Day { get; set; }
         public string? Notes { get; set; } = string.Empty;
-        public string? CancellationReason { get; set; }
     }
 }
