df05449a9eced12fe80a748f610ccaaedcac629a
QuocDai <145921869+dainq94@users.noreply.github.com>
2025-03-25 20:52:12 +0700

    merged duy branch

--- Monhealth.Application/Automapper/ConsultantProfile.cs
+++ Monhealth.Application/Automapper/ConsultantProfile.cs
@@ -18,24 +18,9 @@ namespace Monhealth.Application.Automapper
                 .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.AppUser.PhoneNumber))
                 .ForMember(dest => dest.IsVerified, opt => opt.MapFrom(src => src.IsVerified))
                 .ForMember(dest => dest.BookingCount, opt => opt.MapFrom(src => src.BookingCount));
-
-            CreateMap<Consultant, GetConsultantByIdDTO>()
-                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.AppUser.FullName))
-                .ForMember(dest => dest.Avatar, opt => opt.MapFrom(src => src.AppUser.Avatar))
-                .ForMember(dest => dest.ExpertiseName, opt => opt.MapFrom(src => src.Expertise.ExpertiseName))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.AppUser.Email))
-                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.AppUser.PhoneNumber))
-                .ForMember(dest => dest.IsVerified, opt => opt.MapFrom(src => src.IsVerified))
-                .ForMember(dest => dest.BookingCount, opt => opt.MapFrom(src => src.BookingCount));
-
-
-
-
-
             CreateMap<CreateConsultantDTO, Consultant>().ReverseMap();
             CreateMap<CreateConsultantDTO, Expertise>().ReverseMap();
             CreateMap<CreateConsultantDTO, Certificate>().ReverseMap();
-
             CreateMap<UpdateConsultantDTO, Consultant>().ReverseMap();
         }
     }
