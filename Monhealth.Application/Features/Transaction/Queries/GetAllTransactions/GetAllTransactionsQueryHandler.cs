32c3a4bc39e9ca9218b70a2592be00de05dccd3a
Van Huu Toan <vhtoan27@gmail.com>
2025-03-28 21:51:21 +0700

    Update GetAllTransactionsQueryHandler.cs

--- Monhealth.Application/Features/Transaction/Queries/GetAllTransactions/GetAllTransactionsQueryHandler.cs
+++ Monhealth.Application/Features/Transaction/Queries/GetAllTransactions/GetAllTransactionsQueryHandler.cs
@@ -16,8 +16,7 @@ namespace Monhealth.Application.Features.Transaction.Queries.GetAllTransactions
         }
         public async Task<PageResult<GetAllTransactionsDTO>> Handle(GetAllTransactionsQuery request, CancellationToken cancellationToken)
         {
-            var listTransactions = await _transactionRepository.GetAllTransactionsAsync(request.Page, request.Limit, request.Type, 
-            request.Status, request.Search);
+            var listTransactions = await _transactionRepository.GetAllTransactionsAsync(request.Page, request.Limit, request.Type, request.Search, request.Status);
             var listTransactionsDto = _mapper.Map<List<GetAllTransactionsDTO>>(listTransactions.Items);
             return new PageResult<GetAllTransactionsDTO>()
             {
