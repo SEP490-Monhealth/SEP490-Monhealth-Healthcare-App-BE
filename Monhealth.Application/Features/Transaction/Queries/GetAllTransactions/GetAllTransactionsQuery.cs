255682778c3e7f5d6083fa3bce266adb53209f30
Van Huu Toan <vhtoan27@gmail.com>
2025-03-28 20:23:03 +0700

    feat: change order params transactions

--- Monhealth.Application/Features/Transaction/Queries/GetAllTransactions/GetAllTransactionsQuery.cs
+++ Monhealth.Application/Features/Transaction/Queries/GetAllTransactions/GetAllTransactionsQuery.cs
@@ -9,17 +9,16 @@ namespace Monhealth.Application.Features.Transaction.Queries.GetAllTransactions
         public int Page { get; set; }
         public int Limit { get; set; }
         public TransactionType? Type { get; set; }
-        public StatusTransaction? Status { get; set; }
         public string? Search { get; set; }
+        public StatusTransaction? Status { get; set; }
 
-        public GetAllTransactionsQuery(int page, int limit,
-         TransactionType? type, StatusTransaction? status , string search)
+        public GetAllTransactionsQuery(int page, int limit, TransactionType? type, string search, StatusTransaction? status)
         {
             Page = page;
             Limit = limit;
             Type = type;
-            Status = status;
             Search = search;
+            Status = status;
         }
     }
 }
