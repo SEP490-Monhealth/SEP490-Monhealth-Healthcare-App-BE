c0e90e907b56f484ffe391dd8cb5ed9385d03270
NgPhKhanh <khanh01668309526@gmail.com>
2025-03-28 14:13:58 +0700

    done fix bug getall getbyid getbyconsultantid of consultantBank table

--- Monhealth.Application/Contracts/Persistence/IConsultantBankRepository.cs
+++ Monhealth.Application/Contracts/Persistence/IConsultantBankRepository.cs
@@ -6,6 +6,7 @@ namespace Monhealth.Application.Contracts.Persistence
     public interface IConsultantBankRepository : IGenericRepository<ConsultantBank, Guid>
     {
         Task<PaginatedResult<ConsultantBank>> GetAllConsultantBanksAsync(int page, int limit, string? search, bool? status);
+        Task<ConsultantBank> GetConsultantBankById(Guid consultantBankId);
         Task<List<ConsultantBank>> GetConsultantBankByConsultantIdAsync(Guid consultantId);
         Task<bool> CheckAccountNumber(string? accountNumber);
         Task<int> SaveChangeAsync(CancellationToken cancellationToken);
