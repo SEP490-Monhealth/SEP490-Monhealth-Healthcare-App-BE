2ab8060808b71fbf698323ff579281380dc1996e
Van Huu Toan <vhtoan27@gmail.com>
2025-03-29 13:57:25 +0700

    fix: fix bugs withdrawal status

--- Monhealth.Application/Features/Withdrawal/Commands/CancelWithDrawalStatus/CancelWithDrawalStatusCommandHandler.cs
+++ Monhealth.Application/Features/Withdrawal/Commands/CancelWithDrawalStatus/CancelWithDrawalStatusCommandHandler.cs
@@ -3,7 +3,7 @@ using Monhealth.Domain.Enum;
 
 namespace Monhealth.Application
 {
-    public class CancelWithDrawalStatusCommandHandler(IWithDrawalRepository withDrawalRepository) : IRequestHandler<CancelWithDrawalStatusCommand, Unit>
+    public class CancelWithDrawalStatusCommandHandler(IWithdrawalRepository withDrawalRepository) : IRequestHandler<CancelWithDrawalStatusCommand, Unit>
     {
         public async Task<Unit> Handle(CancelWithDrawalStatusCommand request, CancellationToken cancellationToken)
         {
@@ -14,17 +14,16 @@ namespace Monhealth.Application
                 throw new Exception("Yêu cầu rút tiền không tồn tại.");
             }
 
-            if (status.Status != WithDrawalStatus.Pending)
+            if (status.Status != WithdrawalStatus.Pending)
             {
                 throw new Exception("Chỉ có yêu cầu ở trạng thái 'Pending' mới được phép từ chối.");
             }
 
-            status.Status = WithDrawalStatus.Rejected;
+            status.Status = WithdrawalStatus.Rejected;
             withDrawalRepository.Update(status);
             await withDrawalRepository.SaveChangeASync();
 
             return Unit.Value;
         }
-
     }
 }
--- Monhealth.Application/Features/Withdrawal/Commands/UpdateStatusOfWithDrawal/UpdateStatusWithDrawalCommandHandler.cs
+++ Monhealth.Application/Features/Withdrawal/Commands/UpdateStatusOfWithDrawal/UpdateStatusWithDrawalCommandHandler.cs
@@ -3,27 +3,27 @@ using Monhealth.Domain.Enum;
 
 namespace Monhealth.Application
 {
-    public class UpdateStatusWithDrawalCommandHandler(IWithDrawalRepository withDrawalRepository) : IRequestHandler<UpdateStatusWithDrawalCommand, Unit>
+    public class UpdateStatusWithDrawalCommandHandler(IWithdrawalRepository withDrawalRepository) : IRequestHandler<UpdateStatusWithDrawalCommand, Unit>
     {
         public async Task<Unit> Handle(UpdateStatusWithDrawalCommand request, CancellationToken cancellationToken)
         {
             var withDrawalRequest = await withDrawalRepository.GetByIdAsync(request.WithDrawalRequestId);
             switch (withDrawalRequest.Status) // Assuming 'Status' is the property to check
             {
-                case WithDrawalStatus.Pending:
-                    withDrawalRequest.Status = WithDrawalStatus.Approved;
+                case WithdrawalStatus.Pending:
+                    withDrawalRequest.Status = WithdrawalStatus.Approved;
                     break;
-                case WithDrawalStatus.Approved:
-                    withDrawalRequest.Status = WithDrawalStatus.Completed;
+                case WithdrawalStatus.Approved:
+                    withDrawalRequest.Status = WithdrawalStatus.Completed;
                     break;
-                case WithDrawalStatus.Completed:
+                case WithdrawalStatus.Completed:
                     throw new Exception("Yêu cầu đã hoàn tất, không thể xử lý thêm.");
                 default:
                     throw new Exception("Trạng thái yêu cầu không hợp lệ");
             }
-             withDrawalRepository.Update(withDrawalRequest);
-             await withDrawalRepository.SaveChangeASync();
-             return Unit.Value;
+            withDrawalRepository.Update(withDrawalRequest);
+            await withDrawalRepository.SaveChangeASync();
+            return Unit.Value;
         }
     }
 }
