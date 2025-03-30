8aaef42b20aaa9baa145b4050fb457036ddc2562
NgPhKhanh <khanh01668309526@gmail.com>
2025-03-30 22:11:02 +0700

    done api patch approve withdrawal requests

---
+++ Monhealth.Application/Features/Withdrawal/Commands/ChangeToApproveStatus/ChangeToApproveStatusCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Monhealth.Application.Features.Withdrawal.Commands.ChangeToApproveStatus
+{
+    public class ChangeToApproveStatusCommand : IRequest<bool>
+    {
+        public Guid WithdrawalRequestId { get; set; }
+    }
+}
---
+++ Monhealth.Application/Features/Withdrawal/Commands/ChangeToApproveStatus/ChangeToApproveStatusCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Monhealth.Domain.Enum;
+
+namespace Monhealth.Application.Features.Withdrawal.Commands.ChangeToApproveStatus
+{
+    public class ChangeToApproveStatusCommandHandler(IWithdrawalRepository withdrawalRepository) : IRequestHandler<ChangeToApproveStatusCommand, bool>
+    {
+        public async Task<bool> Handle(ChangeToApproveStatusCommand request, CancellationToken cancellationToken)
+        {
+            var withdrawalRequest = await withdrawalRepository.GetByIdAsync(request.WithdrawalRequestId);
+            if (withdrawalRequest == null)
+            {
+                throw new Exception("Yêu cầu rút tiền không tồn tại.");
+            }
+            if(withdrawalRequest.Status == WithdrawalStatus.Pending)
+            {
+                withdrawalRequest.Status = WithdrawalStatus.Approved;
+            } else
+            {
+                throw new Exception("Không thể thay đổi trạng thái");
+            }
+            withdrawalRepository.Update(withdrawalRequest);
+            await withdrawalRepository.SaveChangeASync();
+            return true;
+        }
+    }
+}
