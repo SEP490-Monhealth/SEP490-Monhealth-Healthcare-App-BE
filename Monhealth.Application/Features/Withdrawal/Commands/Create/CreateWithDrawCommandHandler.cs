ba2e872d7c35ad15a1f130dd6d2243711f31b662
QuocDai <145921869+dainq94@users.noreply.github.com>
2025-04-10 06:13:49 +0700

    added logic check wallet when consultant withdrawRequest

--- Monhealth.Application/Features/Withdrawal/Commands/Create/CreateWithDrawCommandHandler.cs
+++ Monhealth.Application/Features/Withdrawal/Commands/Create/CreateWithDrawCommandHandler.cs
@@ -1,12 +1,27 @@
-using MediatR;
+﻿using MediatR;
+using Monhealth.Application.Contracts.Persistence;
+using Monhealth.Application.Exceptions;
 using Monhealth.Domain;
 
 namespace Monhealth.Application
 {
-    public class CreateWithDrawCommandHandler(IWithdrawalRepository withdrawalRepository) : IRequestHandler<CreateWithdrawalRequestDTO, bool>
+    public class CreateWithDrawCommandHandler(IWithdrawalRepository withdrawalRepository,
+                                                IWalletRepository walletRepository
+        ) : IRequestHandler<CreateWithdrawalRequestDTO, bool>
     {
         public async Task<bool> Handle(CreateWithdrawalRequestDTO request, CancellationToken cancellationToken)
         {
+            var wallet = await walletRepository.GetWalletByConsultantId(request.ConsultantId);
+            if (wallet == null)
+            {
+                throw new BadRequestException("Không tìm thấy ví của chuyên viên tư vấn");
+            }
+
+            if (wallet.Balance < request.Amount)
+            {
+                throw new BadRequestException("Số dư không để để tạo yêu cầu rút tiền");
+            };
+
             WithdrawalRequest model = new WithdrawalRequest
             {
                 WithdrawalRequestId = Guid.NewGuid(),
