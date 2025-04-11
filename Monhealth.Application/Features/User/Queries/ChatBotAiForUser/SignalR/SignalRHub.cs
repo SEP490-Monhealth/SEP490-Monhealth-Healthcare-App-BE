1ada3cbb8bb3e8cdb5e8dea518f49332cf0c354e
Van Huu Toan <vhtoan27@gmail.com>
2025-04-12 00:21:44 +0700

    feat: config signalr auto response when user connect

--- Monhealth.Application/Features/User/Queries/ChatBotAiForUser/SignalR/SignalRHub.cs
+++ Monhealth.Application/Features/User/Queries/ChatBotAiForUser/SignalR/SignalRHub.cs
@@ -4,6 +4,27 @@ namespace Monhealth.Application
 {
     public class SignalRHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            string connectionId = Context.ConnectionId;
+
+            var welcomeMessage = new
+            {
+                messageId = Guid.NewGuid().ToString(),
+                sender = "MonAI",
+                content = new
+                {
+                    generalAdvice = "Chào bạn! Tôi là MonAI, trợ lý ảo của bạn. Tôi có thể giúp gì cho bạn?",
+                    summaryConversation = "",
+                    isHealthOrFitness = false
+                }
+            };
+
+            await Clients.Client(connectionId).SendAsync("ReceiveMessage", welcomeMessage);
+
+            await base.OnConnectedAsync();
+        }
+
         public async Task SendMessageToClient(string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", message);
