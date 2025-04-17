079afac0db6404565270e4fe1bf3da7698d48cfc
QuocDai <145921869+dainq94@users.noreply.github.com>
2025-04-17 20:57:11 +0700

    added consultantBankId field

--- Monhealth.Application/Features/Withdrawal/Commands/Update/UpdateWithDrawalDTO.cs
+++ Monhealth.Application/Features/Withdrawal/Commands/Update/UpdateWithDrawalDTO.cs
@@ -1,9 +1,8 @@
-using MediatR;
-
 namespace Monhealth.Application
 {
-    public class UpdateWithdrawalDTO : IRequest<Boolean>
+    public class UpdateWithdrawalDTO
     {
+        public Guid ConsultantBankId { get; set; }
         public string Description { get; set; } = string.Empty;
         public float Amount { get; set; }
     }
