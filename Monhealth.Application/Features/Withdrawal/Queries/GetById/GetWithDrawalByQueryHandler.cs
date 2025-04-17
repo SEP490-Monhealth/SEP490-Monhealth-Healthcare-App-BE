2887cb7595d9eb8cb5b7b52219fa0e06e1fa46b8
QuocDai <145921869+dainq94@users.noreply.github.com>
2025-04-17 19:44:44 +0700

    fixed get withDrawRequestById

--- Monhealth.Application/Features/Withdrawal/Queries/GetById/GetWithDrawalByQueryHandler.cs
+++ Monhealth.Application/Features/Withdrawal/Queries/GetById/GetWithDrawalByQueryHandler.cs
@@ -4,12 +4,15 @@ using Monhealth.Application.Contracts.Persistence;
 namespace Monhealth.Application
 {
     public class GetWithdrawalByQueryHandler(IWithdrawalRepository withdrawalRepository,
-    IConsultantRepository consultantRepository) : IRequestHandler<GetWithdrawalByIdQuery, GetWithdrawalByIdDTO>
+    IConsultantRepository consultantRepository,
+    IConsultantBankRepository consultantBankRepository
+    ) : IRequestHandler<GetWithdrawalByIdQuery, GetWithdrawalByIdDTO>
     {
         public async Task<GetWithdrawalByIdDTO> Handle(GetWithdrawalByIdQuery request, CancellationToken cancellationToken)
         {
             var query = await withdrawalRepository.GetByIdAsync(request.WithdrawalRequestId);
             var consultant = await consultantRepository.GetConsultantById(query.ConsultantId);
+            var consultantBank = await consultantBankRepository.GetConsultantBankById(query.ConsultantBankId);
             if (query == null) throw new Exception($"không tìm thấy id: {query}");
             return new GetWithdrawalByIdDTO
             {
@@ -23,10 +26,15 @@ namespace Monhealth.Application
                 UpdatedAt = query.UpdatedAt,
                 Consultant = new ConsultantDTOForId
                 {
-                   AvatarUrl = consultant?.AppUser?.Avatar??"",
-                   Email = consultant?.AppUser?.Email??"",
-                   FullName = consultant?.AppUser?.FullName??"",
-                   PhoneNumber = consultant?.AppUser?.PhoneNumber??""
+                    AvatarUrl = consultant?.AppUser?.Avatar ?? "",
+                    Email = consultant?.AppUser?.Email ?? "",
+                    FullName = consultant?.AppUser?.FullName ?? "",
+                    PhoneNumber = consultant?.AppUser?.PhoneNumber ?? ""
+                },
+                ConsultantBank = new ConsultantBankDto
+                {
+                    AccountName = consultantBank?.AccountName ?? "",
+                    AccountNumber = consultantBank?.AccountNumber ?? "",
                 }
             };
         }
