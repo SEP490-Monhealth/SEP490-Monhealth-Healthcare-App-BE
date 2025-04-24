c3c77aee4f080a8ca8665f0aeccfc014a9616f99
NgPhKhanh <khanh01668309526@gmail.com>
2025-04-25 00:00:55 +0700

    done fix remove field meetUrl

--- Monhealth.Application/Features/Consultant/Commands/UpdateConsultant/UpdateConsultantDTO.cs
+++ Monhealth.Application/Features/Consultant/Commands/UpdateConsultant/UpdateConsultantDTO.cs
@@ -4,6 +4,6 @@
     {
         public string Bio { get; set; } = string.Empty;
         public int? Experience { get; set; }
-        public string MeetUrl { get; set; } = string.Empty;
+        //public string MeetUrl { get; set; } = string.Empty;
     }
 }
