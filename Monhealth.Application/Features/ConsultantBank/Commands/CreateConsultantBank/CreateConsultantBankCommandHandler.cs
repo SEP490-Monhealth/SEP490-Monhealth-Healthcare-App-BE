887d57a7669be3d20ac9090a7ff0e871168d2939
DuyPh1472 <saophadao1472@gmail.com>
2025-05-01 00:09:54 +0700

    feat: Add new logic in Create ConsultantBank

--- Monhealth.Application/Features/ConsultantBank/Commands/CreateConsultantBank/CreateConsultantBankCommandHandler.cs
+++ Monhealth.Application/Features/ConsultantBank/Commands/CreateConsultantBank/CreateConsultantBankCommandHandler.cs
@@ -16,28 +16,53 @@ namespace Monhealth.Application.Features.ConsultantBank.Commands.CreateConsultan
             {
                 throw new Exception("Ngân hàng không tồn tại");
             }
+
+            // Check if the account number already exists
             if (await consultantBankRepository.CheckAccountNumber(request.CreateConsultantBankDTO.AccountNumber, null))
             {
                 throw new Exception("Số tài khoản đã tồn tại");
             }
+
+            // Get the current list of banks for the consultant
             var countBanks = await consultantBankRepository.GetConsultantBankByConsultantIdAsync(request.CreateConsultantBankDTO.ConsultantId);
+
+            // Check if the consultant has reached the limit of 5 banks
             if (countBanks.Count() >= 5)
             {
                 throw new BadRequestException("Giới hạn tài khoản của bạn có thể thêm là 5");
-
             }
+
+            // Map the DTO to the domain entity
             var newConsultantBank = mapper.Map<Domain.ConsultantBank>(request.CreateConsultantBankDTO);
-            if (newConsultantBank.IsDefault) //set default if true
+
+            // If it's the first bank, set it as default
+            if (countBanks.Count() == 0)
+            {
+                newConsultantBank.IsDefault = true;
+            }
+            else
+            {
+                // For subsequent banks, use the IsDefault value from the request
+                newConsultantBank.IsDefault = request.CreateConsultantBankDTO.IsDefault;
+            }
+
+            // If it's set to default, update the existing consultant's other banks to non-default
+            if (newConsultantBank.IsDefault)
             {
                 await consultantBankRepository.SetDefaultBankAccountAsync(newConsultantBank.ConsultantId);
             }
+
             newConsultantBank.ConsultantBankId = Guid.NewGuid();
             newConsultantBank.BankId = bank.BankId;
             newConsultantBank.CreatedAt = DateTime.Now;
             newConsultantBank.UpdatedAt = DateTime.Now;
+
+            // Add the new bank account to the repository and save changes
             consultantBankRepository.Add(newConsultantBank);
             await consultantBankRepository.SaveChangeAsync(cancellationToken);
+
             return Unit.Value;
         }
+
     }
 }
